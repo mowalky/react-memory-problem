abe90660b0598c59fdc3757038289c4ccf496f5b
mowalky <mowalky@outlook.com>
2022-11-15 20:40:17 -0600

    updated css

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,11 @@ function App() {
           "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?cs=srgb&dl=pexels-pixabay-45201.jpg&fm=jpg",
         value: "cat",
       },
+      {
+        image:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=0.672xw:1.00xh;0.166xw,0&resize=640:*",
+        value: "dog",
+      },
     ];
     setCards([...defaultCards]);
   };
@@ -29,7 +34,7 @@ function App() {
   return (
     <div className="App">
       {cards ? (
-        cards.map((card) => <div>card</div>)
+        cards.map((card) => <div className="mem-card">{card.image}</div>)
       ) : (
         <div>no memory cards defined</div>
       )}
