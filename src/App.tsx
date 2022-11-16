7a289abac6b47a2ee3be2e343bfbf7405b4af3f7
mowalky <mowalky@outlook.com>
2022-11-15 20:36:42 -0600

    building memory card list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
+type MemCard = {
+  image: string;
+  value: string;
+};
+
 function App() {
   // store memory cards
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState<MemCard[] | null>();
+
+  const createMemoryCards = () => {
+    const defaultCards = [
+      {
+        image:
+          "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?cs=srgb&dl=pexels-pixabay-45201.jpg&fm=jpg",
+        value: "cat",
+      },
+    ];
+    setCards([...defaultCards]);
+  };
+
+  useEffect(() => {
+    createMemoryCards();
+  }, []);
 
   return (
     <div className="App">
-      {cards.length > 0 ? (
+      {cards ? (
         cards.map((card) => <div>card</div>)
       ) : (
         <div>no memory cards defined</div>
